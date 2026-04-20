1a34fe9b81e9545312825434153b5b9b7cf7d5c6
Michael Sholty <mike.sholty@me.com>
2026-04-20 07:38:25 -0400

    Initial Commit

---
+++ runedraw/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  runedraw
+//
+//  Created by Michael Sholty on 4/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ runedraw/runedrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  runedrawApp.swift
+//  runedraw
+//
+//  Created by Michael Sholty on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct runedrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
